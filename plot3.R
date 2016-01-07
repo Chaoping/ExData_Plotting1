543c3d70d9bf3be48b485d7b75bfc6f3d7a91119
Chaoping Guo <Chaoping Guo>
2016-01-07 16:19:10 +0800

    done

---
+++ plot3.R
@@ -0,0 +1,6 @@
+png("plots/plot3.png", width = 480, height = 480)
+plot(times, data$Sub_metering_1, type = "l", ylab = "Energy sub metering")
+points(times, data$Sub_metering_2, col = "red", type = "l")
+points(times, data$Sub_metering_3, col = "blue", type = "l")
+legend("topright", c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), col = c("black", "red", "blue"), lty = 1)
+dev.off()
